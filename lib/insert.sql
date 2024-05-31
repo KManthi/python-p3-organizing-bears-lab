4f53cde53966fb7858f1650a2c96aae6dc6a8344
Daniel <dani3lkamanthi@gmail.com>
2024-05-31 17:25:37 +0300

    Completes Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 16, 'F', 'White', 'Aggressive', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 8, 'F', 'Dark Brown', 'Intense', 0);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 35, 'M', 'Brown', 'Stoic', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 13, 'F', 'Black', 'Playful', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 22, 'M', 'Green', 'Grinchy', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 10, 'F', 'Brown', 'Naive', 1);
+INSERT INTO bears VALUES (8, null, 20, 'M', 'Brown', 'Nice', 0);
